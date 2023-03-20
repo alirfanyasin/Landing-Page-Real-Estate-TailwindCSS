0207556cfb5afb167865eb71adbce222a15fb5c2
alirfanyasin <risayunita21@gmail.com>
2023-03-20 12:54:26 +0700

    finish services-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         heroPattern: "url('/src/images/hero.png')",
         ourStoryPattern: "url('/src/images/bg-OUR STORY.png')",
         servicePattern: "url('/src/images/bg-SERVICES.png')",
+        
+      },
+      backgroundColor: {
+        avatar1: "rgba(136, 51, 255, 0.3)",
+        avatar2: "rgba(204, 116, 41, 0.3)",
+        avatar3: "rgba(230, 46, 123, 0.3)",
       },
       colors: {
         badgeContent1 : '#FF6633',
@@ -16,6 +22,7 @@ module.exports = {
         badgeBgContent3 : 'rgba(51, 97, 255, 0.1)',
         badgeContent4 : '#FF6633',
         badgeBgContent4 : 'rgba(255, 102, 51, 0.1)',
+
       }
     },
   },
